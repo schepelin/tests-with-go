d5bb6a0effc9dc3eb1b384147ed816104d4a9680
Maxim Schepelin <m.schepelin@gmail.com>
2022-02-03 20:29:32 +0100

    Stage 1: Added Arrange-Act-Assert structure

--- game/game_test.go
+++ game/game_test.go
@@ -7,16 +7,20 @@ import (
 )
 
 func TestGame(t *testing.T) {
+	// arrange
 	game := NewGame()
 	player := NewPlayer()
 	player.AddStack(100)
 	assert.Equal(t, 100, player.Stack)
 	player.Join(&game)
 	assert.True(t, player.InGame)
-
 	player.MakeBet(FIVE, 50)
 	assert.Equal(t, 50, player.Stack)
+
+	// act
 	roll := player.RollDie()
+
+	// assert
 	if roll == FIVE {
 		assert.Equal(t, 150, player.Stack)
 	}
